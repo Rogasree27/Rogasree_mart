81dd5e747e5c280c425bfe9ec21dfbef9a371239
rogasree <rogasri79@gmail.com>
2026-09-20 19:24:02 +0530

    Add page routes

---
+++ src/main/java/com/rogasree/rogasreemart/PageController.java
@@ -0,0 +1,43 @@
+package com.rogasree.rogasreemart;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "forward:/login.html";
+    }
+
+    @GetMapping("/seller-login")
+    public String sellerLogin() {
+        return "forward:/seller-login.html";
+    }
+
+    @GetMapping("/admin-login")
+    public String adminLogin() {
+        return "forward:/admin-login.html";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "forward:/register.html";
+    }
+
+    @GetMapping("/cart")
+    public String cart() {
+        return "forward:/cart.html";
+    }
+
+    @GetMapping("/wishlist")
+    public String wishlist() {
+        return "forward:/wishlist.html";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "forward:/home.html";
+    }
+}
