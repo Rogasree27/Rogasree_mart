557ce3080c66f34489d322bbf58ffa5a6071b95a
rogasree <rogasri79@gmail.com>
2026-09-21 10:55:27 +0530

    Add CrossOrigin and forgot password endpoint to SellerController

--- src/main/java/com/rogasree/rogasreemart/SellerController.java
+++ src/main/java/com/rogasree/rogasreemart/SellerController.java
@@ -1,11 +1,14 @@
 package com.rogasree.rogasreemart;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class SellerController {
 
     @Autowired
@@ -18,5 +21,20 @@ public class SellerController {
                 seller.getEmail(),
                 seller.getPassword()
         ).orElse(null);
+    }
+
+    @PostMapping("/seller/forgot-password")
+    public String forgotPassword(@RequestBody Map<String, String> request) {
+        String email = request.get("email");
+        String newPassword = request.get("newPassword");
+
+        Seller seller = sellerRepository.findByEmail(email).orElse(null);
+        if (seller != null) {
+            seller.setPassword(newPassword);
+            sellerRepository.save(seller);
+            return "Password updated successfully";
+        } else {
+            return "Seller email not found";
+        }
     }
 }
