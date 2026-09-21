3885303600905d808d6d8bf5689949b757ff2d34
rogasree <rogasri79@gmail.com>
2026-09-21 21:34:58 +0530

    Add WebController for root redirect and set Railway BASE_URL

---
+++ src/main/java/com/rogasree/rogasreemart/WebController.java
@@ -0,0 +1,13 @@
+package com.rogasree.rogasreemart;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String redirectToLogin() {
+        return "redirect:/admin-login.html";
+    }
+}
